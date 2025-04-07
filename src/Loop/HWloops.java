7d5de86829f2bd111bee12e632634c9e1ff98f3a
Rocio <rociopamela74@hotmail.com>
2025-04-07 10:39:48 -0400

    first commit

---
+++ src/Loop/HWloops.java
@@ -0,0 +1,112 @@
+package Loop;
+
+public class HWloops {
+
+	public static void main(String[] args) {
+		//Homework SWITCH
+				byte month = 8;
+				switch (month)
+				{
+				case 1:
+					System.out.println("January");
+					break;
+				case 2:
+					System.out.println("February");
+					break;
+				case 3:
+					System.out.println("March");
+					break;
+				case 4:
+					System.out.println("April");
+					break;		
+				case 5:
+					System.out.println("May");
+					break;	
+				case 6:
+					System.out.println("June");
+					break;
+				case 7:
+					System.out.println("July");
+					break;	
+				case 8:
+					System.out.println("August");
+					break;
+				case 9:
+					System.out.println("September");
+					break;
+				case 10:
+					System.out.println("October");
+					break;
+				case 11:
+					System.out.println("November");
+					break;
+				case 12:
+					System.out.println("December");
+					break;
+				default:
+				{
+					System.out.println("Please enter a value between 1 to 12");
+				}
+				}
+				
+				//Homework IF LOOP
+				//if month = 1 then print "January"
+				//if month = 2 then print "February"
+				//if month = 1 then print "March"
+				//if month = 1 then print "April"
+				//if month = 1 then print "May"
+				//if month = 1 then print "June"
+				//if month = 1 then print "July"
+				//if month = 1 then print "August"
+				//if month = 1 then print "September"
+				//if month = 1 then print "October"
+				//if month = 1 then print "November"
+				//if month = 1 then print "December"
+				//else print "Please enter a value between 1 to 12"
+				
+				byte monthyear = 12;
+				if (monthyear==1)
+				{
+					System.out.println("January");
+				}else if (monthyear==2)
+				{
+					System.out.println("February");
+				}else if (monthyear==3)
+				{
+					System.out.println("March");
+				}else if (monthyear==4)
+				{
+					System.out.println("April");
+				}else if (monthyear==5)
+				{
+					System.out.println("May");
+				}else if (monthyear==6)
+				{
+					System.out.println("June");
+				}else if (monthyear==7)
+				{
+					System.out.println("July");
+				}else if (monthyear==8)
+				{
+					System.out.println("August");
+				}else if (monthyear==9)
+				{
+					System.out.println("September");
+				}else if (monthyear==10)
+				{
+					System.out.println("October");
+				}else if (monthyear==11)
+				{
+					System.out.println("November");
+				}else if (monthyear==12)
+				{
+					System.out.println("December");
+				}
+				else
+				{
+					System.out.println("Please enter a value between 1 to 12");
+				}
+
+	}
+
+}
